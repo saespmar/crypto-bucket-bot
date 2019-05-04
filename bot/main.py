201aa274c9230afbf470337476f65fd0da3e9001
Samuel Espinosa <saespmar@gmail.com>
2019-05-04 12:19:43 +0200

    Add !price command to the bot

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,12 @@
 import config
 import telegram_api
+import coingecko_api
 
 
 def main():
     bot = telegram_api.TelegramRequester(config.bot_token)
     current_offset = None  # Identifier of the first update to be returned
+    coinGecko = coingecko_api.CoinGeckoRequester()
 
     # Infinite loop doing long polling (30 seconds by default)
     while True:
@@ -20,8 +22,44 @@ def main():
             else:
                 received_text = ''
 
+            # arguments[0] -> command
+            # arguments[1] -> actual arguments
+            arguments = received_text.lower().strip().split(" ", 1)
+
+            # ======================== SIMPLE PRICE ======================== #
+            if (arguments[0] == '!price' and len(arguments) == 2):
+
+                # Convert spaces in the name of the coin into hyphens
+                coin = arguments[1].strip().replace(' ', '-')
+                response = coinGecko.simple_price(coin)
+
+                if len(response) > 0:
+
+                    # Equivalence between currencies and emojis
+                    emoji_map = {'chf': '🇨🇭', 'inr': '🇮🇳', 'eur': '🇪🇺',
+                                 'cad': '🇨🇦', 'aud': '🇦🇺', 'gbp': '🇬🇧',
+                                 'usd': '🇺🇸'}
+                    # Equivalence between currencies and signs
+                    sign_map = {'chf': 'CHF', 'inr': '₹', 'eur': '€',
+                                'cad': '$', 'aud': '$', 'gbp': '£',
+                                'usd': '$'}
+                    output = ''
+
+                    # Build the response
+                    for currency in response:
+                        output = output + \
+                            emoji_map[currency] + ' ' + \
+                            str(response[currency]) + ' ' + \
+                            sign_map[currency] + \
+                            '\n'
+
+                else:
+                    output = arguments[1] + ' not found'
+            else:
+                output = 'Invalid format'
+
             # Reply
-            bot.send_message(chat_id, 'You said: ' + received_text.lower())
+            bot.send_message(chat_id, output)
 
             # Update the offset
             current_offset = update_id + 1
