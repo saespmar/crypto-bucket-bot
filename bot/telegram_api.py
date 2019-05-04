7741cc6c0993bc3d5ffa6d90860b201c3534b066
Samuel Espinosa <saespmar@gmail.com>
2019-05-04 10:42:21 +0200

    Add Telegram requester to get updates and send messages

---
+++ bot/telegram_api.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class TelegramRequester:
+
+    def __init__(self, token):
+        self.token = token
+        self.url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, offset=None, timeout=30):
+        params = {'offset': offset, 'timeout': timeout}
+        response = requests.get(self.url + 'getUpdates', params)
+        return response.json()['result']
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        return requests.post(self.url + 'sendMessage', params)
