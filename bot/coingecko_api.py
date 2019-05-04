feb80b5a1fb5fa255db6c10d0859501f41546750
Samuel Espinosa <saespmar@gmail.com>
2019-05-04 11:50:37 +0200

    Add coingecko api with the 'simple price' request

---
+++ bot/coingecko_api.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class CoinGeckoRequester:
+
+    url_v3 = 'https://api.coingecko.com/api/v3/'
+    commonCurrencies = ['usd', 'eur', 'gbp', 'cad', 'chf', 'aud', 'inr']
+
+    def __init__(self, url=url_v3, currencies=commonCurrencies):
+        self.url = url
+        self.currencies = currencies
+        self.currenciesText = ",".join(currencies)
+
+    def simple_price(self, coin):
+        params = {'ids': coin, 'vs_currencies': self.currenciesText}
+        response = requests.get(self.url + 'simple/price', params)
+        response_json = response.json()
+        if coin in response_json:
+            return response_json[coin]
+        else:
+            return []
